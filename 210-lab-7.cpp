6ba0aab91b108d115b5b6629b0fb66aba75b3111
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-14 22:26:28 -0800

    update

---
+++ 210-lab-7.cpp
@@ -0,0 +1,30 @@
+//*****************************************************************************
+// COMSC-210 | Lab 7 | Gabriel Marquez
+// Description: this program reverses a dynamic string array.
+//*****************************************************************************
+
+#include <iostream>
+using namespace std;
+
+const int SIZE = 5;
+
+void displayArray(string *);
+string* reverseArray(string *);
+
+main()
+{
+    
+
+    return 0;
+}
+
+void displayArray(string *arr)
+{
+    for(int i = 0; i < SIZE; ++i)
+        cout << *(arr + i);
+}
+
+string* (string *arr)
+{
+
+}
